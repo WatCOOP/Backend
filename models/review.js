b3d0f7a92c09fa85d11f6fd2c6855c9f42627416
Mohammed Al-Rasheed <mohammed.satar.alrasheed@gmail.com>
2022-05-22 09:14:56 -0400

    okay

--- models/review.js
+++ models/review.js
@@ -11,7 +11,7 @@ const review = new mongoose.Schema({
         required: true,
     },
     salary: {
-        type: String,
+        type: Number,
         required: true,
     },
     datePosted: {
